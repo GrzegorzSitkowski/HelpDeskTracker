3a84a483fd181428d8f9e9322b3a7623f0b420e8
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-11-16 17:20:17 +0100

    Add project files.

---
+++ HelpDeskTracker.Api/Controllers/HealthChecksController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Api.Controllers
+{
+    [Route("api/hc")]
+    [ApiController]
+    public class HealthChecksController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> GetAsync()
+        {
+            return "Healthy";
+        }
+    }
+}
