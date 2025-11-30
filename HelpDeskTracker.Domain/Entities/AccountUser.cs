fe86ff318b8e14e4bf70202bedad10232902aa03
GrzegorzSitkowski <sitkowskigrzegorz8@gmail.com>
2025-11-30 19:58:37 +0100

    Created entity of AccountUser.cs

---
+++ HelpDeskTracker.Domain/Entities/AccountUser.cs
@@ -0,0 +1,17 @@
+﻿using HelpDeskTracker.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelpDeskTracker.Domain.Entities
+{
+    public class AccountUser : AuditableEntity
+    {
+        public int AccountId { get; set; }
+        public Account Account { get; set; }
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
